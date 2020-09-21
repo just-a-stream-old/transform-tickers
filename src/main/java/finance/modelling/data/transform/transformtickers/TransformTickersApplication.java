c02ba57c883eeb47c60ffbeaaf58f6826e709012
keirbailey <keirinvestment@gmail.com>
2020-09-21 18:39:14 +0100

    Initial commit w/ dependencies

---
+++ src/main/java/finance/modelling/data/transform/transformtickers/TransformTickersApplication.java
@@ -0,0 +1,13 @@
+package finance.modelling.data.transform.transformtickers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransformTickersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransformTickersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/finance/modelling/data/transform/transformtickers/TransformTickersApplicationTests.java
@@ -0,0 +1,13 @@
+package finance.modelling.data.transform.transformtickers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransformTickersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
